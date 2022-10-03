f981472a9d537ff78dcb2f77d5905dff37333897
s-keito <110315888+s-keito@users.noreply.github.com>
2022-10-03 08:39:36 +0000

    [add]userのedit,show,indexとdressのnew,show,index,edit

--- app/controllers/public/dresses_controller.rb
+++ app/controllers/public/dresses_controller.rb
@@ -1,17 +1,43 @@
 class Public::DressesController < ApplicationController
-  def new
+
+   def new
+    @dress = Dress.new
+   end
+
+  def create
+    @dress = Dress.new(dress_params)
+    @dress.user_id = current_user.id
+    if @dress.save
+      redirect_to user_path(@dress.user), notice: "You have created book successfully."
+    else
+      @dresses = Dress.all
+      render 'index'
+    end
   end
 
   def show
+    @dress = Dress.find(params[:id])
   end
 
   def index
+    @dresses = Dress.all.order(created_at: :desc)
   end
 
   def edit
+    @dress = Dress.find(params[:id])
+  end
+
+  def update
+    @dress = Dress.find(params[:id])
+    if @dress.update(dress_params)
+      redirect_to dress_path(@dress), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
   end
 
   private
+
   def dress_params
     params.require(:dress).permit(:image, :category, :caption)
   end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,31 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @user = User.find(params[:id])
+    # @seasons = Season.all
+    # @dresses = params[:season_id].present? ? Season.find(params[:season_id]).dresses : Dress.all
   end
 
   def index
+    @users = User.all.order(created_at: :desc)
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :user_image, :season_id, :seasonal)
   end
 end
