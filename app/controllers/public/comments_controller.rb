6ee4568c5f4a68516ab63f839933cebf27ef9188
s-keito <110315888+s-keito@users.noreply.github.com>
2022-10-11 09:41:17 +0000

    [add]トップ、アバウト完成

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,15 +1,14 @@
 class Public::CommentsController < ApplicationController
   def create
     dress = Dress.find(params[:dress_id])
-    comment = current_user.comments.new(comment_params)
-    comment.dress_id = dress.id
-    comment.save
-    redirect_to dress_path(dress)
+    @comment = current_user.comments.new(comment_params)
+    @comment.dress_id = dress.id
+    @comment.save
   end
 
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to dress_path(params[:dress_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
   end
 
   private
