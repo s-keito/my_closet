6cce91140697bf3dc1263a57060ea56c5253cc89
s-keito <110315888+s-keito@users.noreply.github.com>
2022-10-17 13:07:32 +0000

    [add]syntaxerroe解消

--- db/migrate/20221007005354_add_status_to_dresses.rb
+++ db/migrate/20221007005354_add_status_to_dresses.rb
@@ -1,5 +1,5 @@
 class AddStatusToDresses < ActiveRecord::Migration[6.1]
   def change
-    add_column :dresses, :status, :integur, null: false, default: 0
+    add_column :dresses, :status, :integer, null: false, default: 0
   end
 end
