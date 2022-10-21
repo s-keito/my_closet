e93a7664fa19524075d22b50ce1e887e98773c67
s-keito <110315888+s-keito@users.noreply.github.com>
2022-10-21 01:54:56 +0000

    [add]seachで非表示を検索できないようにする変更

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -7,7 +7,7 @@ class Public::SearchesController < ApplicationController
     if @range == "User"
       @users = User.looks(params[:word])
     else
-      @dresses = Dress.looks(params[:word])
+      @dresses = Dress.looks(params[:word]).where(is_status: true)
     end
   end
 end
