7bfd537c98f0ec952c7f6763d87cd6ff009ce968
s-keito <110315888+s-keito@users.noreply.github.com>
2022-10-24 06:42:19 +0000

    [add]カテゴリー追加seed削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,3 +28,20 @@ Category.create([
     { kind: 'accessories' },
     ])
 
+User.create!(
+  [
+    {email: 'a@a', name: 'Aさん', password: 'aaaaaa', introduction: 'こんにちは', user_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+    {email: 'b@b', name: 'Bさん', password: 'bbbbbb', introduction: 'アンニョン', user_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email: 'c@c', name: 'Cさん', password: 'cccccc', introduction: 'ハロー', user_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
+  ]
+)
+
+# Dress.create!(
+#   [
+#     {image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.png"), filename:"sample-post1.png"), caption: 'ユニクロ　2000円', user_id: 1, season_id: 2, category_id: 1 },
+#     {image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.png"), filename:"sample-post2.png"), caption: 'gu　1000円', user_id: 1, season_id: 1, category_id: 1 },
+#     {image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.png"), filename:"sample-post3.png"), caption: 'adidas　1500円', user_id: 1, season_id: 2, category_id: 2 },
+#     {image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post4.png"), filename:"sample-post4.png"), caption: '無印良品　7000円', user_id: 1, season_id: 4, category_id: 1 }
+#   ]
+# )
+
