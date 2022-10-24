84000095a1ab7308c348aec69b9916e5af344544
s-keito <110315888+s-keito@users.noreply.github.com>
2022-10-24 07:34:54 +0000

    [add]room編集

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -23,11 +23,11 @@ class Public::RoomsController < ApplicationController
     @room = Room.find(params[:id])
 
     unless @room.entries.where(user_id: params[:user_id]).any?
-      redirect_to user_room_path
+      #redirect_to user_room_path
     end
 
     unless @room.entries.where(user_id: current_user.id).any?
-      redirect_to user_room_path
+      #redirect_to user_room_path
     end
 
     @messages = @room.messages.all
