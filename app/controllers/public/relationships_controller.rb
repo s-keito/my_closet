9189778398e876ec78d586353525bd2e51835b0b
s-keito <110315888+s-keito@users.noreply.github.com>
2022-11-01 12:04:19 +0000

    [add]フォロー機能非同期か

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,11 +2,13 @@ class Public::RelationshipsController < ApplicationController
   # フォローする
   def create
     current_user.follow(params[:user_id])
+    @user = User.find(params[:user_id])
     # redirect_to request.referer
   end
   # フォロー外すとき
   def destroy
     current_user.unfollow(params[:user_id])
+    @user = User.find(params[:user_id])
     # redirect_to request.referer
   end
   # フォロー一覧
